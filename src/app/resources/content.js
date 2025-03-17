29644738d394b447f462c16fc2dd6caa909a3ea1
Max Klonke <klonkem@umich.edu>
2025-03-17 01:25:06 -0400

    Taking off languages

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -9,7 +9,7 @@ const person = {
   role: "Software Engineer",
   avatar: "/images/avatar.jpg",
   location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  languages: [], // Just leaving this array empty because I don't want to display languages
 };
 
 const newsletter = {
