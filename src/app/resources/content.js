c8daf8ed0ba0fdfe384598e87da4285cea7f614d
Max Klonke <klonkem@umich.edu>
2025-03-24 20:09:05 -0400

    Fixed github link

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -30,7 +30,7 @@ const social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://https://github.com/klonkem",
+    link: "https://github.com/klonkem",
   },
   {
     name: "LinkedIn",
